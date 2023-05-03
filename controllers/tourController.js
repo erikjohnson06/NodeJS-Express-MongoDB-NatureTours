b4aa430f875928d13370515ced1cdae11f251619
Erik Johnson <erikjohnson06@gmail.com>
2023-05-03 18:22:43 -0400

    Implementing CRUD operations to MongoDB

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,91 +21,123 @@ exports.checkId = (request, response, next, val) => {
 };
 
 //Middleware to check for required fields for a new tour record
-exports.checkBody = (request, response, next) => {
+//exports.checkBody = (request, response, next) => {
+//
+//    console.log("checkBody:" , request.body, typeof (request.body.name), typeof (request.body.price));
+//    if (typeof (request.body.name) === "undefined" || typeof (request.body.price) === "undefined") {
+//    //if (!request.body.name || !request.body.price) {
+//        return response
+//                .status(400)
+//                .json({
+//                    status: "error",
+//                    message: "Invalid Tour Name or Price"
+//                });
+//    }
+//
+//    next();
+//};
 
-    console.log("checkBody:" , request.body, typeof (request.body.name), typeof (request.body.price));
-    if (typeof (request.body.name) === "undefined" || typeof (request.body.price) === "undefined") {
-    //if (!request.body.name || !request.body.price) {
-        return response
-                .status(400)
+exports.getAllTours = async (request, response) => {
+
+    try {
+
+        const tours = await Tour.find();
+
+        response
+                .status(200)
                 .json({
-                    status: "error",
-                    message: "Invalid Tour Name or Price"
+                    status: "success",
+                    results: tours.length,
+                    data: {
+                        tours: tours
+                    }
                 });
+    } catch (e) {
+        response.status(400).json({
+            status: "fail",
+            message: "An unexpected error has occurred",
+            error: e
+        });
     }
-
-    next();
 };
 
-exports.getAllTours = (request, response) => {
+exports.getTourById = async (request, response) => {
 
-    //console.log("requestTime: ", request.requestTime);
+    try {
 
-    response
-            .status(200)
-            .json({
-                status: "success",
-//                results: tours.length,
-//                data: {
-//                    tours: tours
-//                }
-            });
+        const tour = await Tour.findById(request.params.id);
+        //const tour = await Tour.findOne({_id: request.params.id })
+
+        response
+                .status(200)
+                .json({
+                    status: "success",
+                    data: {
+                        tour: tour
+                    }
+                });
+    } catch (e) {
+        response.status(400).json({
+            status: "error",
+            message: "An unexpected error has occurred",
+            error: e
+        });
+    }
 };
 
-exports.getTourById = (request, response) => {
+exports.createTour = async (request, response) => {
 
-    //console.log(request.params);
-    const id = parseInt(request.params.id);
-//    const tour = tours.find(el => el.id === id);
+    try {
 
-    response
-            .status(200)
-            .json({
-                status: "success",
-                message: "",
-//                data: {
-//                    tour: tour
-//                }
-            });
+        console.log(request.body);
+
+        //const tour = new Tour({});
+        const tour = await Tour.create(request.body); //Returns Promise
+
+        response.status(201).json({
+            status: "success",
+            message: "Tour Created Successfully",
+            data: {
+                tour: tour
+            }
+        });
+    } catch (e) {
+        response.status(400).json({
+            status: "error",
+            message: "Data validation failed!",
+            error: e
+        });
+    }
 };
 
-exports.createTour = (request, response) => {
+exports.updateTourById = async (request, response) => {
 
-    console.log(request.body);
+    try {
 
-//    const newId = tours[tours.length - 1].id + 1;
-//    const newTour = Object.assign({id: newId}, request.body);
-//
-//    tours.push(newTour);
-//
-//    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-//        response.status(201).json({
-//            status: "success",
-//            message: "",
-//            data: {
-//                tour: newTour
-//            }
-//        });
-//    });
-};
+        const tour = await Tour.findByIdAndUpdate(request.params.id, request.body, {
+            new: true, //return newly updated document
+            runValidators : true
+        });
 
-exports.updateTourById = (request, response) => {
-
-    const id = parseInt(request.params.id);
-//    const tour = tours.find(el => el.id === id);
-
-    response.status(200).json({
-        status: 'success',
-        data: {
-            tour: "<updated tour here>"
-        }
-    });
+        response.status(201).json({
+            status: "success",
+            message: "Tour Updated Successfully",
+            data: {
+                tour: tour
+            }
+        });
+    } catch (e) {
+        response.status(400).json({
+            status: "error",
+            message: "An unexpected error has occurred",
+            error: e
+        });
+    }
 };
 
 exports.deleteTourById = (request, response) => {
 
     const id = parseInt(request.params.id);
-//    const tour = tours.find(el => el.id === id);
 
     response.status(204).json({
         status: "success",
